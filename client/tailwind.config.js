7f6ca0236d3fc2fa0b8aad95c9f41011825cdaf5
Naomi Cherotich <naomirono1454@gmail.com>
2023-11-26 18:50:17 +0300

    Add the paths to all of your template files in your tailwind.config.js file.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx}",
+  "./public/index.html"],
   theme: {
     extend: {},
   },
