271d01aa73e30645ed6740469282ebe690b5dec0
Naomi Cherotich <naomirono1454@gmail.com>
2023-11-29 14:32:12 +0300

    Adjust the height of the section and add a div with filters

--- client/src/components/Layout/Hero/Hero.jsx
+++ client/src/components/Layout/Hero/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section
       id="about"
-      className="py-14 mx-auto items-center relative"
+      className="py-8 mx-auto items-center relative h-[90vh]"
       style={{
         backgroundImage: "url('/src/assets/catering1.jpg')",
         backgroundSize: 'cover',
@@ -16,7 +16,7 @@ const Hero = () => {
 
       <div className="container max-w-[1100px] mx-auto md:my-8 md:px-4 lg:px-0 px-4 relative z-10">
         <div className="flex flex-col md:flex-row items-center justify-center">
-          <div className="md:w-1/2 mt-8 mb-24 md:mt-0 md:ml-20 text-white">
+          <div className="md:w-1/2 mt-8 mb-28 md:mt-0 md:ml-20 text-white">
             <h2 className="text-5xl font-extrabold mb-4">
               Unleash Culinary Magic with Chef Connect
             </h2>
@@ -51,7 +51,7 @@ const Hero = () => {
               <img
                 src="/src/assets/chef2.jpg"
                 alt="Image"
-                className="w-full md:w-5/6 h-[550px] md:max-w-md ml-[80px] hero-img"
+                className="w-full md:w-5/6 h-[500px] md:max-w-md ml-[80px] hero-img"
               />
               <img
                 src="/src/assets/Chefbg9.png"
@@ -62,6 +62,48 @@ const Hero = () => {
           </div>
         </div>
       </div>
+      <div className="flex items-center justify-center z-10 absolute bottom-[-45px] right-[250px]">
+  <div className="bg-white bg-opacity-40 backdrop-filter backdrop-blur-l p-4 rounded shadow-lg max-w-[1100px] mx-auto ">
+    <h2 className="text-3xl font-bold mb-4">Chef Connect</h2>
+
+    {/* Filters */}
+    <div className="flex items-center space-x-4">
+      <select
+        className=" px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-white bg-opacity-40 backdrop-filter backdrop-blur-l"
+      >
+        <option value="">Select Cuisine</option>
+        {/* Add your cuisine options here */}
+      </select>
+
+      <select
+        className=" px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-white bg-opacity-40 backdrop-filter backdrop-blur-l"
+      >
+        <option value="">Select Rating</option>
+        {/* Add your rating options here */}
+      </select>
+
+      <select
+        className=" px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-white bg-opacity-40 backdrop-filter backdrop-blur-l"
+      >
+        <option value="">Select Availability</option>
+        {/* Add your availability options here */}
+      </select>
+
+      <input
+        type="text"
+        placeholder="Enter Location"
+        className=" px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-white bg-opacity-40 backdrop-filter backdrop-blur-l"
+      />
+
+      {/* Get Started Button */}
+      <button className="bg-blue-500 text-white py-2 px-6 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+        Search
+      </button>
+    </div>
+  </div>
+</div>
+
+
     </section>
   );
 };
