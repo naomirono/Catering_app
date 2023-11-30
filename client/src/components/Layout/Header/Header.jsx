069faf506357f1a19f20435dbc74702181bfee62
Naomi Cherotich <naomirono1454@gmail.com>
2023-11-30 13:13:02 +0300

    Replace the h1 title with a logo image and edit the navlinks

--- client/src/components/Layout/Header/Header.jsx
+++ client/src/components/Layout/Header/Header.jsx
@@ -1,5 +1,6 @@
 // src/components/Header.js
 import React, { useState } from 'react';
+import LogoImage from '/src/assets/CateringLogo2.png'
 
 const Header = () => {
   const [isAuthenticated, setAuthenticated] = useState(false);
@@ -23,15 +24,17 @@ const Header = () => {
     <header className="bg-black text-white p-4 ">
       <div className="container mx-auto custom-container flex items-center justify-between">
         <div>
-          <h1 className="text-xl font-semibold">Chef Connect</h1>
+        
+            <img src={LogoImage} alt="Chef Connect Logo" className="h-12 w-auto" />
+          
         </div>
 
         <nav>
           <ul className="flex space-x-4">
-            <li><a href="/" className="hover:underline">Home</a></li>
-            <li><a href="/about" className="hover:underline">About</a></li>
-            <li><a href="/contact" className="hover:underline">Contact</a></li>
-            {/* Add more navigation links as needed */}
+            <li><a href="/" className="hover:text-orange-500 hover:underline">Home</a></li>
+            <li><a href="/about" className="hover:text-orange-500 hover:underline">Chef</a></li>
+            <li><a href="/about" className="hover:text-orange-500 hover:underline">How It Works</a></li>
+            <li><a href="/contact" className="hover:text-orange-500 hover:underline">Pricing</a></li>
           </ul>
         </nav>
 
@@ -44,11 +47,11 @@ const Header = () => {
             </>
           ) : (
             <>
-              <button onClick={handleLogin} className="bg-white text-blue-500 px-4 py-2 rounded-full focus:outline-none">
-                Sign In
+              <button onClick={handleLogin} className="border text-white px-6 py-1.5 rounded-full focus:outline-none">
+                log In
               </button>
-              <button onClick={handleSignUp} className="bg-white text-blue-500 px-4 py-2 ml-4 rounded-full focus:outline-none">
-                Sign Up
+              <button onClick={handleSignUp} className="bg-orange-500 text-white px-4 py-1.5 ml-6 rounded-full focus:outline-none">
+                Join Us
               </button>
             </>
           )}
