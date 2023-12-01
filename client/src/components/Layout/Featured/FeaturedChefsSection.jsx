9f2af1555851c7c4b599df7eda23e6652726ead9
Naomi Cherotich <naomirono1454@gmail.com>
2023-12-01 19:43:19 +0300

    Add a bg image and a z-index to the section

--- client/src/components/Layout/Featured/FeaturedChefsSection.jsx
+++ client/src/components/Layout/Featured/FeaturedChefsSection.jsx
@@ -4,9 +4,16 @@ import { chefs } from './FeaturedData';
 
 const FeaturedChefsSection = () => {
   return (
-    <section className=" py-12">
-      <div className="container mx-auto max-w-[1100px]">
-        <h2 className="text-5xl font-bold text-center mt-12 mb-4 text-white">Featured <span className='text-orange-500'>Chefs</span> </h2>
+    <section className=" py-12 relative"
+    style={{
+        backgroundImage: "url('/src/assets/catering1.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}>
+      <div className="absolute inset-0 bg-black opacity-90"></div>
+
+      <div className="container mx-auto max-w-[1100px] relative z-10">
+        <h2 className="text-5xl font-bold text-center mt-4 mb-4 text-white">Featured <span className='text-orange-500'>Chefs</span> </h2>
         <p className="text-center text-gray-500 mb-12 max-w-[900px] mx-auto">
           Meet our talented chefs who bring a world of flavors to your doorstep. Each chef is a culinary artist, ready to create a memorable dining experience for you and your guests. Explore their profiles and discover the perfect chef to elevate your next meal.
         </p>
