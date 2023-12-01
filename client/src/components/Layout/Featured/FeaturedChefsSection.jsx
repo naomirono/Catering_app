e3d7156bf6b1065423fc355f05826434de311a50
Naomi Cherotich <naomirono1454@gmail.com>
2023-12-01 10:20:42 +0300

    Delete the mock up data

--- client/src/components/Layout/Featured/FeaturedChefsSection.jsx
+++ client/src/components/Layout/Featured/FeaturedChefsSection.jsx
@@ -1,34 +1,8 @@
 import React from 'react';
 import FeaturedChef from './FeaturedChef';
+import { chefs } from './FeaturedData';
 
 const FeaturedChefsSection = () => {
-  const chefs = [
-    {
-      name: 'Chef John Doe',
-      expertise: 'Italian Cuisine',
-      location: 'Mombasa, Kenya',
-      imageSrc: '/src/assets/chef2.jpg',
-    },
-    {
-      name: 'Chef Jane Smith',
-      expertise: 'Asian Fusion',
-      location: 'Nairobi, Kenya',
-      imageSrc: '/src/assets/CateringF1.jpg',
-    },
-    {
-        name: 'Chef John Doe',
-        expertise: 'Swahili Cuisine',
-        location: 'Mombasa, Kenya',
-        imageSrc: '/src/assets/CateringF2.jpg',
-      },
-      {
-        name: 'Chef Jane Smith',
-        expertise: 'Kikuyu Delicacies',
-        location: 'Nakuru, Kenya',
-        imageSrc: '/src/assets/chef2.jpg',
-      }
-  ];
-
   return (
     <section className=" py-12">
       <div className="container mx-auto max-w-[1100px]">
