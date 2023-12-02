ca6125006cfcd93533fa7880b16cfcddc81df1de
Naomi Cherotich <naomirono1454@gmail.com>
2023-12-02 14:04:04 +0300

    Add a time to mockup data and a date and time paragraph to card

--- client/src/components/Layout/Blog/Blog.jsx
+++ client/src/components/Layout/Blog/Blog.jsx
@@ -9,26 +9,30 @@ const Blog = () => {
       content: 'Master the art of grilling with these expert tips. From choosing the right charcoal to achieving the perfect sear, Chef John Doe shares his secrets to elevate your barbecue game.',
       author: 'Chef John Doe',
       date: 'December 15, 2023',
+      time:'5 min',
     },
     {
       title: 'Delicious Holiday Dessert Recipes',
       content: 'Satisfy your sweet tooth this holiday season with these delightful dessert recipes. Chef Jane Smith showcases a variety of festive treats that will impress your guests and leave them craving more.',
       author: 'Chef Jane Smith',
       date: 'December 10, 2023',
+      time:'6 min',
     },
     {
       title: 'Behind the Scenes: A Day in the Life of a Culinary Artist',
       content: 'Ever wondered what goes on behind the kitchen doors? Join Chef Alex Johnson on a culinary journey as he provides a glimpse into the hectic yet rewarding life of a professional chef.',
       author: 'Chef Alex Johnson',
       date: 'December 5, 2023',
+      time:'10 min',
     },
   ];
 
   return (
     <section className="py-16">
       <div className="container mx-auto max-w-[1100px]">
-        <div className='text-center mb-8'>
-          <h2 className="text-5xl font-bold text-white">Chef Tips and <span className='text-orange-500'>Recipes</span></h2>
+      <div className='text-center mb-8'>
+          <h2 className="text-5xl font-bold text-white mb-4">Chef Tips and <span className='text-orange-500'>Recipes</span></h2>
+          <p className="text-lg text-gray-500 mb-6">Indulge in culinary excellence with Chef Tips and Recipes – your passport to a world of delicious creations. From grilling mastery to festive treats, our chefs unlock the secrets behind extraordinary meals. Elevate your kitchen experience and savor the journey of every dish!</p>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -38,7 +42,8 @@ const Blog = () => {
               <div className="flex-grow overflow-hidden">
                 <p className="text-gray-600">{post.content}</p>
               </div>
-              <p className="text-sm text-gray-500 mt-4">{`By ${post.author} on ${post.date}`}</p>
+              <p className="text-sm text-gray-500 mt-4">{post.author}</p>
+              <p className="text-sm text-gray-500">{`${post.date} . ${post.time} read`}</p>
               <div className="mt-4">
                 <a
                   href="#"
