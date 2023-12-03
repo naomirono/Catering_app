bc3077237c35df7ca05b25591387695b4c6eb016
Naomi Cherotich <naomirono1454@gmail.com>
2023-12-03 14:12:19 +0300

    Add a profile image to the left of author paragraph

--- client/src/components/Layout/Blog/Blog.jsx
+++ client/src/components/Layout/Blog/Blog.jsx
@@ -2,35 +2,38 @@
 
 import React from 'react';
 
-const Blog = () => {
+const BlogRecipeSection = () => {
   const blogPosts = [
     {
       title: '10 Tips for Perfecting Your Grilling Skills',
       content: 'Master the art of grilling with these expert tips. From choosing the right charcoal to achieving the perfect sear, Chef John Doe shares his secrets to elevate your barbecue game.',
       author: 'Chef John Doe',
       date: 'December 15, 2023',
-      time:'5 min',
+      time: '5 min',
+      profileImage: 'https://placekitten.com/50/50', // Replace with actual image URL
     },
     {
       title: 'Delicious Holiday Dessert Recipes',
       content: 'Satisfy your sweet tooth this holiday season with these delightful dessert recipes. Chef Jane Smith showcases a variety of festive treats that will impress your guests and leave them craving more.',
       author: 'Chef Jane Smith',
       date: 'December 10, 2023',
-      time:'6 min',
+      time: '6 min',
+      profileImage: 'https://placekitten.com/50/50', // Replace with actual image URL
     },
     {
       title: 'Behind the Scenes: A Day in the Life of a Culinary Artist',
       content: 'Ever wondered what goes on behind the kitchen doors? Join Chef Alex Johnson on a culinary journey as he provides a glimpse into the hectic yet rewarding life of a professional chef.',
       author: 'Chef Alex Johnson',
       date: 'December 5, 2023',
-      time:'10 min',
+      time: '10 min',
+      profileImage: 'https://placekitten.com/50/50', // Replace with actual image URL
     },
   ];
 
   return (
     <section className="py-16">
       <div className="container mx-auto max-w-[1100px]">
-      <div className='text-center mb-8'>
+        <div className='text-center mb-8'>
           <h2 className="text-5xl font-bold text-white mb-4">Chef Tips and <span className='text-orange-500'>Recipes</span></h2>
           <p className="text-lg text-gray-500 mb-6">Indulge in culinary excellence with Chef Tips and Recipes – your passport to a world of delicious creations. From grilling mastery to festive treats, our chefs unlock the secrets behind extraordinary meals. Elevate your kitchen experience and savor the journey of every dish!</p>
         </div>
@@ -42,7 +45,14 @@ const Blog = () => {
               <div className="flex-grow overflow-hidden">
                 <p className="text-gray-600">{post.content}</p>
               </div>
-              <p className="text-sm text-gray-500 mt-4">{post.author}</p>
+              <div className="flex items-center mt-4">
+                <img
+                  src={post.profileImage}
+                  alt={`Profile of ${post.author}`}
+                  className="w-8 h-8 rounded-full mr-2"
+                />
+                <p className="text-sm text-gray-500">{post.author}</p>
+              </div>
               <p className="text-sm text-gray-500">{`${post.date} . ${post.time} read`}</p>
               <div className="mt-4">
                 <a
@@ -60,4 +70,4 @@ const Blog = () => {
   );
 };
 
-export default Blog;
+export default BlogRecipeSection;
