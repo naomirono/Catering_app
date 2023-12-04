3a0775c48e394e28b260e1b73c97b62874944f8b
Naomi Cherotich <naomirono1454@gmail.com>
2023-12-04 22:15:25 +0300

    Delete commented lines

--- client/src/components/Layout/Featured/FeaturedMenu.jsx
+++ client/src/components/Layout/Featured/FeaturedMenu.jsx
@@ -1,5 +1,3 @@
-// FeaturedMenus.js
-
 import React from 'react';
 
 const menusData = [
@@ -10,7 +8,7 @@ const menusData = [
     description: 'Treat your taste buds to our Skewed Chicken Breast, expertly prepared and grilled to perfection. Chef Sarah Kamau infuses bold flavors, creating a dish thats both succulent and savory.',
     price: '$27.99',
     chef: 'Chef Ahmed Ali',
-    image: '/src/assets/CateringMenu6.png', // Replace with actual image URL
+    image: '/src/assets/CateringMenu6.png', 
   },
   {
     id: 2,
@@ -18,7 +16,7 @@ const menusData = [
     description: 'Indulge in the rich flavors of Nyama Choma, a traditional Kenyan barbecue dish. Our chef grills premium cuts of meat to perfection, seasoned with a secret blend of Kenyan spices.',
     price: '$30.99',
     chef: 'Chef Wanjiku Njoroge',
-    image: '/src/assets/CateringMenu1.png', // Replace with actual image URL
+    image: '/src/assets/CateringMenu1.png', 
   },
   {
     id: 3,
@@ -26,7 +24,7 @@ const menusData = [
     description: 'Delight in our Meatballs Spaghetti Extravaganza, a culinary masterpiece by Chef Michael Omondi. Enjoy the perfect blend of Italian-inspired spaghetti and savory meatballs, topped with rich tomato sauce.',
     price: '$32.99',
     chef: 'Chef Amina Odhiambo',
-    image: '/src/assets/CateringMenu3..png', // Replace with actual image URL
+    image: '/src/assets/CateringMenu3..png', 
   },
 ];
 
