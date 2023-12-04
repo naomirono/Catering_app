3a06a4843182a520289e8d5aeb82f5aeb073efb5
Naomi Cherotich <naomirono1454@gmail.com>
2023-12-04 13:22:25 +0300

    Increase the image width and height

--- client/src/components/Layout/Featured/FeaturedMenu.jsx
+++ client/src/components/Layout/Featured/FeaturedMenu.jsx
@@ -49,7 +49,7 @@ const FeaturedMenus = () => {
             <div key={menu.id} className="py-2 flex">
               {menu.id === 1 && (
                 <>
-                  <img src={menu.image} alt={menu.title} className="w-[200px] h-[200px] object-cover mr-8 " />
+                  <img src={menu.image} alt={menu.title} className="w-[210px] h-[210px] object-cover mr-8 " />
                   <div style={{ marginLeft: '40px' }}>
                     <h3 className="text-xl font-semibold mb-2 text-white ">{menu.title}</h3>
                     <p className="text-gray-600 mb-4">{menu.description}</p>
@@ -67,12 +67,12 @@ const FeaturedMenus = () => {
                     <p className="text-sm text-gray-500 mt-2 hover:text-orange-500" onClick={() => handleViewProfile(menu.chef)}>{`By ${menu.chef}`}</p>
                 
                   </div>
-                  <img src={menu.image} alt={menu.title} className="w-[200px] h-[200px] object-cover ml-8 rounded" />
+                  <img src={menu.image} alt={menu.title} className="w-[210px] h-[210px] object-cover ml-8 rounded" />
                 </>
               )}
               {menu.id === 3 && (
                 <>
-                  <img src={menu.image} alt={menu.title} className="w-[200px] h-[200px] object-cover mr-8 rounded" />
+                  <img src={menu.image} alt={menu.title} className="w-[250px] h-[250px] object-cover mr-8 rounded" />
                   <div style={{ marginLeft: '40px' }}>
                     <h3 className="text-xl font-semibold mb-2 text-white ">{menu.title}</h3>
                     <p className="text-gray-600 mb-4">{menu.description}</p>
