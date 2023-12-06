0b626c3f1e5a081722c8914d30d498cd595ccc0f
Naomi Cherotich <naomirono1454@gmail.com>
2023-12-06 12:49:26 +0300

    Add a bg image and reduce the size of the section

--- client/src/components/Layout/NewsLetter/Newsletter.jsx
+++ client/src/components/Layout/NewsLetter/Newsletter.jsx
@@ -17,27 +17,37 @@ const NewsletterSubscription = () => {
   };
 
   return (
-    <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-4">Subscribe to Our Newsletter</h2>
-      <p className="text-gray-600 mb-4">
-        Stay updated on new chefs, promotions, and cooking tips. Don't miss out on the latest culinary experiences!
-      </p>
-      <div className="flex items-center">
-        <input
-          type="email"
-          placeholder="Your Email"
-          className="p-2 border border-gray-300 rounded-l focus:outline-none"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <button
-          className="bg-orange-500 text-white px-4 py-2 rounded-r hover:bg-orange-600"
-          onClick={handleSubscribe}
-        >
-          Subscribe
-        </button>
+    <section className="py-16">
+      <div className="container mx-auto max-w-[1000px] relative rounded-lg shadow-md text-center"
+        style={{
+          backgroundImage: "url('/src/assets/catering1.jpg')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}>
+        <div className="absolute inset-0 bg-black opacity-80"></div>
+        <div className="p-16 relative z-10 flex flex-col items-center justify-center max-w-[800px] mx-auto">
+          <h2 className="text-3xl font-bold mb-4 text-white">Subscribe to Our Newsletter </h2>
+          <p className="text-gray-400 mb-14">
+            Stay updated on new chefs, promotions, and cooking tips. Don't miss out on the latest culinary experiences!
+          </p>
+          <div className="flex items-center">
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="p-2 border border-gray-300 rounded-l focus:outline-none w-[400px]"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            <button
+              className="bg-orange-500 text-white px-4 py-2.5 rounded-r hover:bg-orange-600"
+              onClick={handleSubscribe}
+            >
+              Subscribe
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
