3fd35e30b09b30852e7a329537ae0e2217ef1ea7
Naomi Cherotich <naomirono1454@gmail.com>
2023-12-12 20:50:29 +0300

    Delete unused react import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/Pages/LandingPage/LandingPage';
 import Login from './components/Pages/Login';
