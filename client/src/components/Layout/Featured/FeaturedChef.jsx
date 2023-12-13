fbf9247fabd2cc2fc3dc40a95f3ff17a00dcd234
Naomi Cherotich <naomirono1454@gmail.com>
2023-12-13 18:02:17 +0300

    Add margin on small devices

--- client/src/components/Layout/Featured/FeaturedChef.jsx
+++ client/src/components/Layout/Featured/FeaturedChef.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FeaturedChef = ({ name, expertise, location, imageSrc }) => {
   return (
-    <div className="bg-orange-500 rounded-lg shadow-lg text-white rounded-xl">
+    <div className="bg-orange-500 rounded-lg shadow-lg text-white rounded-xl mx-6 lg:mx-0">
       <img src={imageSrc} alt={`${name} - Chef`} className='rounded-[10px]' />
       <div className=' pt-4'>
         <h3 className="text-xl font-semibold mb-2 px-4">{name}</h3>
